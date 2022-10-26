7fcab8980d2da3cc47a9b1db2974c2eb1706c2fa
Mega Handayani <megahandayani95@gmail.com>
2022-10-26 23:34:19 +0700

    perbaikan login

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqgN6iUM4dKP8vi8iV4fsggZnZp4K2W7A",
+  authDomain: "newstl-c6c23.firebaseapp.com",
+  projectId: "newstl-c6c23",
+  storageBucket: "newstl-c6c23.appspot.com",
+  messagingSenderId: "746886550235",
+  appId: "1:746886550235:web:dce139eda28ee29464f2fe",
+  measurementId: "G-X5XNSCCYKP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,8 +3,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -12,23 +11,15 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Mega1998
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
 
 const theme = createTheme();
 
-export const SignInSide =()=> {
+export const Login =()=> {
+  const navigate =useNavigate();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -36,6 +27,8 @@ export const SignInSide =()=> {
       email: data.get('email'),
       password: data.get('password'),
     });
+
+    navigate("/");
   };
 
   return (
@@ -102,21 +95,24 @@ export const SignInSide =()=> {
               >
                 Sign In
               </Button>
-              <Grid container>
-                
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
+              
+              <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link to="/register">
+                      Already have an account? Sign up
+                    </Link>
+                  </Grid>
               </Grid>
-              <Copyright sx={{ mt: 5 }} />
+             
             </Box>
           </Box>
         </Grid>
+        
       </Grid>
+      
     </ThemeProvider>
+    
   );
 }
 
-export default SignInSide;
+export default Login;
